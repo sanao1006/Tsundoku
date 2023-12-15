6b5405e8db8f145de8e204e58c610276d4a6bdbe
sanao1006 <naosak1006@gmail.com>
2023-12-15 21:20:05 +0900

    feat: repositoryをDIした
    ViewModel内で使うため

---
+++ app/src/main/java/app/sanao1006/tsundoku/data/di/TsundokuRepository.kt
@@ -0,0 +1,37 @@
+package app.sanao1006.tsundoku.data.di
+
+import app.sanao1006.tsundoku.data.db.BookDao
+import app.sanao1006.tsundoku.data.model.Book
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class TsundokuRepository @Inject constructor(
+    private val bookDao: BookDao,
+    @IODispatcher private val ioDispatcher: CoroutineDispatcher
+) {
+    suspend fun getBooks(): List<Book> {
+        return withContext(ioDispatcher) {
+            return@withContext bookDao.getBooks().map {
+                Book(
+                    id = it.id,
+                    description = it.description ?: "",
+                    title = it.title ?: "",
+                    totalPage = it.totalPage,
+                    nowPage = it.nowPage,
+                    createAt = it.createdAt,
+                    updatedAt = it.updatedAt
+                )
+            }
+        }
+    }
+    suspend fun insertBook(book: Book) = withContext(ioDispatcher) {
+        bookDao.insertBook(book = book)
+    }
+
+    suspend fun updateBookInfo(book: Book) = withContext(ioDispatcher) {
+        bookDao.updateBookInfo(book = book)
+    }
+}
