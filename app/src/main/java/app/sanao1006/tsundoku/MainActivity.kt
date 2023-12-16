feb7ce757dd00c6026ed52419c506fafd958d628
sanao1006 <naosak1006@gmail.com>
2023-12-16 21:38:20 +0900

    feat: メインの画面作ったりした

--- app/src/main/java/app/sanao1006/tsundoku/MainActivity.kt
+++ app/src/main/java/app/sanao1006/tsundoku/MainActivity.kt
@@ -6,9 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import app.sanao1006.tsundoku.data.desiginsystem.TsundokuTheme
+import app.sanao1006.tsundoku.presentation.mainscreen.TsundokuScreen
+import app.sanao1006.tsundoku.presentation.mainscreen.TsundokuScreenViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +28,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
+                    TsundokuApp()
                 }
             }
+        }
+    }
+}
+
+@Composable
+private fun TsundokuApp(viewModel: TsundokuScreenViewModel = hiltViewModel()) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "tsundokus") {
+        composable(route = "tsundokus") {
+            TsundokuScreen(
+                viewModel = viewModel,
+                onFabClick = { },
+                onItemClick = { }
+            )
         }
     }
 }
---
+++ app/src/main/java/app/sanao1006/tsundoku/presentation/mainscreen/TsundokuScreenViewModel.kt
@@ -0,0 +1,36 @@
+package app.sanao1006.tsundoku.presentation.mainscreen
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import app.sanao1006.tsundoku.data.di.TsundokuRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class TsundokuScreenViewModel @Inject constructor(
+    private val tsundokuRepository: TsundokuRepository
+) : ViewModel() {
+    private val _tsundokuState = MutableStateFlow(
+        TsundokuMainScreenState(
+            tsundokus = listOf(),
+            isLoading = true
+        )
+    )
+    val tsundokuState = _tsundokuState.asStateFlow()
+
+    init {
+        getBooks()
+    }
+
+    private fun getBooks() {
+        viewModelScope.launch {
+            _tsundokuState.value = TsundokuMainScreenState(
+                tsundokus = tsundokuRepository.getBooks(),
+                isLoading = false,
+            )
+        }
+    }
+}
